34077f502784fdf738fb520de9bee427c5eae3cd
unknown <komukal@hotmail.com>
2017-09-06 10:31:18 +0300

    moikka

---
+++ robert.php
@@ -0,0 +1,17 @@
+<div id="robert" class="modal bottom-sheet">
+          <div class="modal-content">
+            <h4>Bio</h4>
+            <ul class="collection">
+              <li class="collection-item avatar">
+                <img src="img/dp/robertbw01.jpg" alt="" class="circle">
+                <span class="title">Robert Ehrnsten</span>
+                <p>Photography <br>
+                   Second Line</p>
+			  </li>
+              
+            </ul>
+			      <a href="#!" class=" modal-action modal-close waves-effect red waves-red btn-flat">Stäng</a>
+
+          </div>
+        </div>
+		
